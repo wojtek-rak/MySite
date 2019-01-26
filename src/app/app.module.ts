82a8e459f79670acdedf89b0b4882c6368db0711
Wolix <Rak.wojtek@wp.pl>
2019-01-26 23:40:58 +0100

    Markdown responsive + small fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,7 +46,7 @@ import {MarkdownModule, MarkedOptions} from 'ngx-markdown';
         useValue: {
           gfm: true,
           tables: true,
-          breaks: false,
+          breaks: true,
           pedantic: false,
           sanitize: false,
           smartLists: true,
