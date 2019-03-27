433859eeeba5022006483393f338b92ab5236070
Wojciech Rak <rak.wojtek@wp.pl>
2019-03-27 22:28:14 +0100

    basic galerry

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Gallery, GalleryConfig, GalleryItem, GalleryRef, ImageItem} from '@ngx-gallery/core';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 import {delay} from 'q';
 import {NavigationEnd, Router} from '@angular/router';
 
@@ -26,39 +27,174 @@ export class GalleryComponent implements OnInit, OnDestroy {
     // });
   }
 
-  galleryUNIT_TESTING: GalleryItem[];
-  galleryMVC: GalleryItem[];
-  galleryUNITY_TESTING: GalleryItem[];
-  galleryGAMETESTING: GalleryItem[];
+  //galleryUNIT_TESTING: GalleryItem[];
+  //galleryMVC: GalleryItem[];
+  //galleryUNITY_TESTING: GalleryItem[];
+  //galleryGAMETESTING: GalleryItem[];
+
+
+  galleryOptions: NgxGalleryOptions[];
+  galleryUNIT_TESTING: NgxGalleryImage[];
+  galleryMVC: NgxGalleryImage[];
+  galleryUNITY_TESTING: NgxGalleryImage[];
+  galleryGAMETESTING: NgxGalleryImage[];
 
   ngOnInit() {
+
+    this.galleryOptions = [
+      {
+        width: '600px',
+        height: '500px',
+        thumbnailsColumns: 3,
+        imageAnimation: NgxGalleryAnimation.Slide
+      },
+      // max-width
+      {
+        breakpoint: 1150,
+        width: '600px',
+        height: '500px',
+        //imagePercent: 80,
+        //thumbnailsPercent: 20,
+        //thumbnailsMargin: 20,
+        //thumbnailMargin: 20
+      },
+      {
+        breakpoint: 601,
+        width: '100%',
+        height: '100%'
+        //imagePercent: 80,
+        //thumbnailsPercent: 20,
+        //thumbnailsMargin: 20,
+        //thumbnailMargin: 20
+      },
+      {
+        breakpoint: 450,
+        height: '80%',
+        width: '100%'
+      }
+    ];
+
     this.galleryUNIT_TESTING = [
-      new ImageItem({src: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_1.png', thumb: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_1_MINI.png'}),
-      new ImageItem({src: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_2.jpg', thumb: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_2_MINI.jpg'}),
-      new ImageItem({src: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_3.jpg', thumb: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_3_MINI.jpg'}),
-      new ImageItem({src: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_4.jpg', thumb: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_4_MINI.jpg'}),
-      new ImageItem({src: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_5.jpg', thumb: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_5_MINI.jpg'})
+      {
+        small: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_1_MINI.png',
+        medium: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_1.png',
+        big: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_1.png'
+      },
+      {
+        small: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_2_MINI.jpg',
+        medium: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_2.jpg',
+        big: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_2.jpg'
+      },
+      {
+        small: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_3_MINI.jpg',
+        medium: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_3.jpg',
+        big: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_3.jpg'
+      },
+      {
+        small: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_4_MINI.jpg',
+        medium: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_4.jpg',
+        big: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_4.jpg'
+      },
+      {
+        small: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_5_MINI.jpg',
+        medium: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_5.jpg',
+        big: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_5.jpg'
+      }
     ];
 
     this.galleryMVC = [
-      new ImageItem({src: 'assets/img/gallery/MVC/MVC_4.png', thumb: 'assets/img/gallery/MVC/MINI/MVC_4_MINI.png'}),
-      new ImageItem({src: 'assets/img/gallery/MVC/MVC_1.JPG', thumb: 'assets/img/gallery/MVC/MINI/MVC_1_MINI.jpg'}),
-      new ImageItem({src: 'assets/img/gallery/MVC/MVC_2.JPG', thumb: 'assets/img/gallery/MVC/MINI/MVC_2_MINI.jpg'}),
-      new ImageItem({src: 'assets/img/gallery/MVC/MVC_3.JPG', thumb: 'assets/img/gallery/MVC/MINI/MVC_3_MINI.jpg'})
+      {
+        small: 'assets/img/gallery/MVC/MINI/MVC_4_MINI.png',
+        medium: 'assets/img/gallery/MVC/MVC_4.png',
+        big: 'assets/img/gallery/MVC/MVC_4.png'
+      },
+      {
+        small: 'assets/img/gallery/MVC/MINI/MVC_1_MINI.jpg',
+        medium: 'assets/img/gallery/MVC/MVC_1.JPG',
+        big: 'assets/img/gallery/MVC/MVC_1.JPG'
+      },
+      {
+        small: 'assets/img/gallery/MVC/MINI/MVC_2_MINI.jpg',
+        medium: 'assets/img/gallery/MVC/MVC_2.JPG',
+        big: 'assets/img/gallery/MVC/MVC_2.JPG'
+      },
+      {
+        small: 'assets/img/gallery/MVC/MINI/MVC_3_MINI.jpg',
+        medium: 'assets/img/gallery/MVC/MVC_3.JPG',
+        big: 'assets/img/gallery/MVC/MVC_3.JPG'
+      }
     ];
 
     this.galleryUNITY_TESTING = [
-      new ImageItem({src: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_1.jpg', thumb: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_1_MINI.jpg'}),
-      new ImageItem({src: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_2.JPG', thumb: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_2_MINI.jpg'}),
-      new ImageItem({src: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_3.jpg', thumb: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_3_MINI.jpg'}),
-      new ImageItem({src: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_4.jpg', thumb: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_4_MINI.jpg'})
+      {
+        small: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_1_MINI.jpg',
+        medium: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_1.jpg',
+        big: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_1.jpg'
+      },
+      {
+        small: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_2_MINI.jpg',
+        medium: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_2.JPG',
+        big: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_2.JPG'
+      },
+      {
+        small: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_3_MINI.jpg',
+        medium: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_3.jpg',
+        big: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_3.jpg'
+      },
+      {
+        small: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_4_MINI.jpg',
+        medium: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_4.jpg',
+        big: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_4.jpg'
+      }
     ];
 
     this.galleryGAMETESTING = [
-      new ImageItem({src: 'assets/img/gallery/GAMETESTING/GAMETESTING_1.jpg', thumb: 'assets/img/gallery/GAMETESTING/MINI/GAMETESTING_1_MINI.jpg'}),
-      new ImageItem({src: 'assets/img/gallery/GAMETESTING/GAMETESTING_2.jpg', thumb: 'assets/img/gallery/GAMETESTING/MINI/GAMETESTING_2_MINI.jpg'}),
-      new ImageItem({src: 'assets/img/gallery/GAMETESTING/GAMETESTING_3.jpg', thumb: 'assets/img/gallery/GAMETESTING/MINI/GAMETESTING_3_MINI.jpg'})
+      {
+        small: 'assets/img/gallery/GAMETESTING/MINI/GAMETESTING_1_MINI.jpg',
+        medium: 'assets/img/gallery/GAMETESTING/GAMETESTING_1.jpg',
+        big: 'assets/img/gallery/GAMETESTING/GAMETESTING_1.jpg'
+      },
+      {
+        small: 'assets/img/gallery/GAMETESTING/MINI/GAMETESTING_2_MINI.jpg',
+        medium: 'assets/img/gallery/GAMETESTING/GAMETESTING_2.jpg',
+        big: 'assets/img/gallery/GAMETESTING/GAMETESTING_2.jpg'
+      },
+      {
+        small: 'assets/img/gallery/GAMETESTING/MINI/GAMETESTING_3_MINI.jpg',
+        medium: 'assets/img/gallery/GAMETESTING/GAMETESTING_3.jpg',
+        big: 'assets/img/gallery/GAMETESTING/GAMETESTING_3.jpg'
+      }
     ];
+
+
+
+    // this.galleryUNIT_TESTING = [
+    //   new ImageItem({src: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_1.png', thumb: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_1_MINI.png'}),
+    //   new ImageItem({src: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_2.jpg', thumb: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_2_MINI.jpg'}),
+    //   new ImageItem({src: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_3.jpg', thumb: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_3_MINI.jpg'}),
+    //   new ImageItem({src: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_4.jpg', thumb: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_4_MINI.jpg'}),
+    //   new ImageItem({src: 'assets/img/gallery/UNIT_TESTING/UNIT_TESTING_5.jpg', thumb: 'assets/img/gallery/UNIT_TESTING/MINI/UNIT_TESTING_5_MINI.jpg'})
+    // ];
+
+    // this.galleryMVC = [
+    //   new ImageItem({src: 'assets/img/gallery/MVC/MVC_4.png', thumb: 'assets/img/gallery/MVC/MINI/MVC_4_MINI.png'}),
+    //   new ImageItem({src: 'assets/img/gallery/MVC/MVC_1.JPG', thumb: 'assets/img/gallery/MVC/MINI/MVC_1_MINI.jpg'}),
+    //   new ImageItem({src: 'assets/img/gallery/MVC/MVC_2.JPG', thumb: 'assets/img/gallery/MVC/MINI/MVC_2_MINI.jpg'}),
+    //   new ImageItem({src: 'assets/img/gallery/MVC/MVC_3.JPG', thumb: 'assets/img/gallery/MVC/MINI/MVC_3_MINI.jpg'})
+    // ];
+
+    // this.galleryUNITY_TESTING = [
+    //   new ImageItem({src: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_1.jpg', thumb: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_1_MINI.jpg'}),
+    //   new ImageItem({src: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_2.JPG', thumb: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_2_MINI.jpg'}),
+    //   new ImageItem({src: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_3.jpg', thumb: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_3_MINI.jpg'}),
+    //   new ImageItem({src: 'assets/img/gallery/UNITY_TESTING/UNITY_TESTING_4.jpg', thumb: 'assets/img/gallery/UNITY_TESTING/MINI/UNITY_TESTING_4_MINI.jpg'})
+    // ];
+
+    // this.galleryGAMETESTING = [
+    //   new ImageItem({src: 'assets/img/gallery/GAMETESTING/GAMETESTING_1.jpg', thumb: 'assets/img/gallery/GAMETESTING/MINI/GAMETESTING_1_MINI.jpg'}),
+    //   new ImageItem({src: 'assets/img/gallery/GAMETESTING/GAMETESTING_2.jpg', thumb: 'assets/img/gallery/GAMETESTING/MINI/GAMETESTING_2_MINI.jpg'}),
+    //   new ImageItem({src: 'assets/img/gallery/GAMETESTING/GAMETESTING_3.jpg', thumb: 'assets/img/gallery/GAMETESTING/MINI/GAMETESTING_3_MINI.jpg'})
+    // ];
   }
 
   ngOnDestroy() {
