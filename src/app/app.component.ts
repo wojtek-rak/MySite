921048ecbb8434c7fcea7819296cd1ddfc3d939a
Wojciech Rak <rak.wojtek@wp.pl>
2019-04-07 22:10:04 +0200

    remove animation from gallery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { trigger, transition, group, query, style, animate } from '@angular/anim
   styleUrls: ['./app.component.css'],
   animations: [
     trigger('routeAnimation', [
-      transition('1 => 2, 1 => 3, 1 => 4, 1 => 5, 2 => 3, 2 => 4, 2 => 5, 3 => 4, 3 => 5, 4 => 5', [
+      transition('1 => 2, 1 => 4, 1 => 5, 2 => 4, 2 => 5, 4 => 5', [
         style({height: '!'}),
         query(':enter', style({transform: 'translateX(100%)'})),
         query(':enter, :leave', style({position: 'absolute', top: 0, left: 0, right: 0})),
@@ -16,7 +16,7 @@ import { trigger, transition, group, query, style, animate } from '@angular/anim
           query(':enter', [animate('0.3s cubic-bezier(.35, 0, .25, 1)', style({ transform: 'translateX(0)' })),]),
           ])
         ]),
-      transition('5 => 4, 5 => 3, 5 => 2, 5 => 1, 4 => 3, 4 => 2, 4 => 1, 3 => 2, 3 => 1, 2 => 1', [
+      transition('5 => 4, 5 => 2, 5 => 1, 4 => 2, 4 => 1, 2 => 1', [
         style({height: '!'}),
         query(':enter', style({transform: 'translateX(-100%)'})),
         query(':enter, :leave', style({position: 'absolute', top: 0, left: 0, right: 0})),
